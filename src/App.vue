6af52a866e8d414761b17aaa0328799095b2c479
linxiaoxi <wesifan@163.com>
2022-06-05 17:15:17 +0800

    day1

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+
+  <div id="app">
+    <Header></Header>
+    我是根组件
+    <Footer></Footer>
+  </div>
+</template>
+
+<script>
+import Header from './components/header/Header.vue'
+import Footer from './components/footer/Footer.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Header,
+    Footer,
+  }
+}
+</script>
+
+<style>
+
+</style>
