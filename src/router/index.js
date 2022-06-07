843d62d6675f3d78a343eb9a64a52f8f5310cb10
linxiaoxi <wesifan@163.com>
2022-06-07 16:23:22 +0800

    10重写push.replace方法

--- src/router/index.js
+++ src/router/index.js
@@ -8,6 +8,30 @@ import Home from '@/pages/Home'
 import Search from '@/pages/Search'
 import Login from '@/pages/Login'
 import Register from '@/pages/Register'
+
+let originPush = VueRouter.prototype.push
+let originReplace = VueRouter.prototype.push
+// 重写push|replace
+// 第一个参数：告诉原来push方法，你往哪里跳转（传递哪些参数）、
+// 第二个参数：成功的回调
+// 第三个参数：失败的回调
+// call||apply 的区别
+// 相同点：都可以调用函数一次，都可以篡改函数的上下文一次
+// 不同点:call与apply传递参数：call传递参数用逗号隔开，apply方法执行，传递数组
+VueRouter.prototype.push = function(location,resolve,reject){
+    if(resolve && reject ) {
+        originPush.call(this, location, resolve, reject)
+    }else{
+        originPush.call(this,location, ()=>{},()=>{})
+    }
+}
+VueRouter.prototype.replace = function(location, resolve, reject) {
+    if(resolve && reject ) {
+        originReplace.call(this, location, resolve, reject)
+    }else{
+        originReplace.call(this, location, ()=>{},()=>{})
+    }
+}
 export default new VueRouter ({
     routes:[
         {
