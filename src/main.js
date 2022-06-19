1163bf0d7bfeb15b528d977ee3da30b87d7f47e1
linxiaoxi <wesifan@163.com>
2022-06-19 18:38:29 +0800

    19动态展示三级联动数据

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 // 三级联动组件 ----注册全局组件
-import TypeNav from '@/pages/Home/TypeNav'
+import TypeNav from '@/components/TypeNav'
 // 第一个参数：全局组件的名字 第二个参数：哪一个组件
 Vue.component(TypeNav.name,TypeNav);
 
@@ -11,9 +11,9 @@ Vue.config.productionTip = false
 import router from '@/router';
 // 引入仓库
 import store from '@/store'
-
-import {reqCategoryList} from '@/api'
-reqCategoryList();
+// // 测试
+// import {reqCategoryList} from '@/api'
+// reqCategoryList();
 new Vue({
   render: h => h(App),
   // 注册路由：底下的写法KV一致省略V【router小写的】
