aef377ba36e87f42a0ed04a0d3830acc1eaa0979
linxiaoxi <wesifan@163.com>
2022-07-01 18:56:41 +0800

    33获取Banner轮播图的数据

--- src/store/home/index.js
+++ src/store/home/index.js
@@ -1,12 +1,17 @@
-import { reqCategoryList } from "@/api";
+import { reqCategoryList, reqGetBannerList } from "@/api";
 // home模块的小仓库
 const state = {
     // state中数据默认初始值别瞎写，服务器返回对象，服务器返回数组。【根据接口返回值初始化的】
     categoryList:[],
+    // 轮播图的数据
+    bannerList:[]
 };
 const mutations = {
     CATEGORYLIST(state, categoryList) {
         state.categoryList = categoryList;
+    },
+    GETBANNERLIST(state,bannerList) {
+        state.bannerList = bannerList;
     }
 };
 const actions = {
@@ -16,6 +21,13 @@ const actions = {
         // console.log(result);
         if(result.code==200){
             commit("CATEGORYLIST",result.data);
+        }
+    },
+    // 获取首页轮播图的数据
+    async getBannerList({commit}) {
+        let result = await reqGetBannerList();
+        if(result.code==200){
+            commit('GETBANNERLIST',result.data);
         }
     }
 };
