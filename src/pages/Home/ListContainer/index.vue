7e8999cd96e20f77c50c01b46b089ad7a809404c
linxiaoxi <wesifan@163.com>
2022-07-02 17:48:50 +0800

    36轮播图通过watch+nextTick解决问题

--- src/pages/Home/ListContainer/index.vue
+++ src/pages/Home/ListContainer/index.vue
@@ -118,27 +118,42 @@ export default {
     // 因为dispatch当中涉及到异步语句,导致v-for遍历的时候结构还没有完全因此不行
 
     // console.log('初始化swiper实例')
-    setTimeout(() => {
-      var mySwiper = new Swiper(document.querySelector(".swiper-container"), {
-        loop: true,
-        // 如果需要分页器
-        pagination: {
-          el: ".swiper-pagination",
-          // 点击小球的时候也切换图片
-          clickable: true,
-        },
-        // 如果需要前进后退按钮
-        navigation: {
-          nextEl: ".swiper-button-next",
-          prevEl: ".swiper-button-prev",
-        },
-      });
-    },1000);
+    // setTimeout(() => {}, 1000);
   },
   computed: {
     ...mapState({
       bannerList: (state) => state.home.bannerList,
     }),
+  },
+  watch: {
+    // 监听bannerList数据的变化:因为这条数据发生过变化----由空数组变为数组里面有四个元素
+    bannerList: {
+      handler(newValue, oldValue) {
+        // 现在咱们通过watch监听bannerList属性的属性值的变化
+        // 如果执行handler方法,代表组件实例身上这个属性的属性值已经有了[数组:四个元素]
+        // 当前这个函数执行：只能保证bannerList数据已经有了，但是你没办法保证v-for已经执行结束了
+        // v-for执行完毕，才有结构【你现在在watch当中没办法保证的]
+        // nextTick：在下次DOM更新 循环结束之后 执行延迟回调。 在 修改数据之后 立即使用这个方法，获取更新后的DOM
+        this.$nextTick(() => {
+          // 当你执行这个回调的时候：保证服务器数据回来了，v-for执行完毕了【一定轮播图的结构有了】
+        var mySwiper = new Swiper(document.querySelector(".swiper-container"), {
+          loop: true,
+          // 如果需要分页器
+          pagination: {
+            el: ".swiper-pagination",
+            // 点击小球的时候也切换图片
+            clickable: true,
+          },
+          // 如果需要前进后退按钮
+          navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+          },
+        });
+        })
+        
+      },
+    },
   },
 };
 </script>
