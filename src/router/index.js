aa70f70a9331997f65868ef64705707f9a77f09e
linxiaoxi <wesifan@163.com>
2022-07-18 16:58:02 +0800

    59 Detail静态组件+滚动行为

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,9 @@
 //配置路由的地方
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import routes from './routes';
 
 Vue.use(VueRouter)
-
-import Home from '@/pages/Home'
-import Search from '@/pages/Search'
-import Login from '@/pages/Login'
-import Register from '@/pages/Register'
 
 let originPush = VueRouter.prototype.push
 let originReplace = VueRouter.prototype.push
@@ -33,41 +29,10 @@ VueRouter.prototype.replace = function(location, resolve, reject) {
     }
 }
 export default new VueRouter ({
-    routes:[
-        {
-            path:'/home',
-            component:Home,
-            meta:{show:true}
-        },
-        {
-            // :keyword占位符，？代表参数可传可不传
-            path:'/search/:keyword?',
-            component:Search,
-            meta:{show:true},
-            name:'search',
-            // 路由组件能不能传递props数据？
-            // 布尔值写法:params
-            // props:true,
-            // 对象写法：额外的给路由组件传递一些props
-            // props:{a:1,b:2}
-            // 函数写法：可以params参数、query参数，通过props传递给路由组件
-            props:($route)=>{({keyword:$route.params.keyword,k:$route.query.k})}
-        },
-        {
-            path:'/login',
-            component:Login,
-            meta:{show:false}
-        },
-        {
-            path:'/register',
-            component:Register,
-            meta:{show:false}
-        },
-        // 重定向，项目跑起来，访问/，立马定向到首页
-
-        {
-            path:'*',
-            redirect:'/home'
-        }
-    ]
+    routes,
+    // 滚动行为
+    scrollBehavior (to, from, savedPosition) {
+        // 返回的这个y=0, 代表的滚动条在最上方
+        return { y:0 }
+    }
 })
